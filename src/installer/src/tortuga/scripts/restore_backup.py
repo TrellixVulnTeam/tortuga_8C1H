16e932fed53d8b9ac0ed1ab2382df4ef9cc16176
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 22:58:02 +0000

    Adding tarfile member sanitization to extractall()

--- src/installer/src/tortuga/scripts/restore_backup.py
+++ src/installer/src/tortuga/scripts/restore_backup.py
@@ -86,7 +86,26 @@ class RestoreBackup(object):
         )
 
         with tarfile.open(self.archive_path, 'r:bz2') as archive:
-            archive.extractall(self.restore_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, self.restore_path)
 
     def load_manifest(self) -> None:
         """
